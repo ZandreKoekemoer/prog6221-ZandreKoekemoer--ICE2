258788f9bf7bd7ff6803c9030abd90781ac371b5
RDavidK <160715787+RDavidK@users.noreply.github.com>
2025-08-04 13:43:05 +0200

    Added last object

--- ICE2/Program.cs
+++ ICE2/Program.cs
@@ -13,7 +13,16 @@
             Console.WriteLine("Car Power : " + car[0] + " hp");
             Console.WriteLine("Car Weight : " + car["weight"] + " kgs");
             Console.WriteLine("Top Speed : " + car[2] + " kph");
-            
+
+            Cars dreamcar = new Cars(502, 1418, 293, "911 gt3", "Porsche");
+
+            Console.WriteLine("---------------Cars---------------\n");
+
+            Console.WriteLine("Brand : " + dreamcar[4]);
+            Console.WriteLine("Model : " + dreamcar["model"]);
+            Console.WriteLine("Car Power : " + dreamcar[0] + " hp");
+            Console.WriteLine("Car Weight : " + dreamcar["weight"] + " kgs");
+            Console.WriteLine("Top Speed : " + dreamcar[2] + " kph");
         }
     }
 }
