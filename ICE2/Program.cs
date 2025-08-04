81a10ccc381b36aa92f0f05d155f7e01ee78eac4
RDavidK <160715787+RDavidK@users.noreply.github.com>
2025-08-04 13:33:27 +0200

    Added Display
    Added an example to the main program

--- ICE2/Program.cs
+++ ICE2/Program.cs
@@ -4,6 +4,15 @@
     {
         static void Main(string[] args)
         {
+            Cars car = new Cars(1500, 850, 250, "T-34", "Aurus");
+
+            Console.WriteLine("---------------Cars---------------\n");
+
+            Console.WriteLine("Brand : " + car[4]);
+            Console.WriteLine("Model : " + car["model"]);
+            Console.WriteLine("Car Power : " + car[0] + " hp");
+            Console.WriteLine("Car Weight : " + car["weight"] + " kgs");
+            Console.WriteLine("Top Speed : " + car[2] + " kph");
             
         }
     }
--- Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
+++ Test/obj/Debug/net8.0/Test.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aed6d69037dda87e97acdb2db400d164196c2ac9")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
